b31b0c2b9c60399145ee3b7a367c50c29a430c44
Kyu G. Shim <40845323+kyugshim@users.noreply.github.com>
2019-08-26 20:47:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -46,9 +46,11 @@ async function app() {
   document.getElementById('class-a').addEventListener('click', () => addExample(0));
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
   document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById('class-d').addEventListener('click', () => addExample(3));
 
   while (true) {
     if (classifier.getNumClasses() > 0.5) {
+      console.log(classifier.getNumClasses());
       // Get the activation from mobilenet from the webcam.
       const activation = net.infer(webcamElement, 'conv_preds');
       // Get the most likely class and confidences from the classifier module.
